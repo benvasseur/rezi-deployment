aa592966c043afac5979070ce4c2340c24f388b6
Benjamin Vasseur <benjaminvasseur@lviscorp.com>
2024-10-20 13:18:12 +0900

    add styling and animation on redirect handler

--- src/components/RedirectHandler.tsx
+++ src/components/RedirectHandler.tsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 const RedirectHandler = () => {
   const { shortid } = useParams();
   const [error, setError] = useState<string | null>(null);
+  const [dots, setDots] = useState<string>('.');
+  const [url, setUrl] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchUrl = async () => {
@@ -13,22 +15,52 @@ const RedirectHandler = () => {
           setError('URL not found');
         } else {
           const data = await response.json();
-          window.location.href = data.url;
+          // Let user see cool loading animation
+          setTimeout(() => {
+            setUrl(data.url);
+            // window.location.href = data.url;
+          }, 1000);
         }
       } catch (err) {
         setError('An error occurred while fetching the URL');
       }
     };
 
-    // fetchUrl();
+    fetchUrl();
   }, [shortid]);
 
+  useEffect(() => {
+    if (url || error) return;
+
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + '.' : '.'));
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, [error, url]);
+
   return (
-    <div className="container mx-auto p-4">
+    <div 
+      id="redirect-handler-page" 
+      className="container mx-auto p-4 pb-0 h-screen w-screen max-w-none cursor-wait flex flex-col justify-between text-white"
+    >
       {error ? (
-        <div className="text-red-500">{error}</div>
+        <div className="h-full flex flex-col justify-center">
+          <div className="mx-auto text-3xl text-red-500">{error}</div>
+        </div>
       ) : (
-        <div>Redirecting...</div>
+        <>
+          <div className="h-full flex flex-col justify-center">
+            <div className="mx-auto text-3xl min-w-40">
+              {`Redirecting${dots}`}
+            </div>
+          </div>
+          <img 
+            className="mx-auto mb-0 min-h-52"
+            src="/images/loading.gif" 
+            alt="Loading animation" 
+          />
+        </>
       )}
     </div>
   );
